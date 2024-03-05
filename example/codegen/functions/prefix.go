033f448872f176ae82b8007c5e907ada5ec052e4
Toan Nguyen <hgiasac@gmail.com>
2024-03-06 00:53:17 +0700

    improve cmd/ndc-go-sdk docs and template (#13)

--- example/codegen/functions/prefix.go
+++ example/codegen/functions/prefix.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/hasura/ndc-codegen-example/types"
-	"github.com/hasura/ndc-sdk-go/utils"
 )
 
 type Text string
@@ -141,91 +140,5 @@ type GetTypesArguments struct {
 }
 
 func FunctionGetTypes(ctx context.Context, state *types.State, arguments *GetTypesArguments) (*GetTypesArguments, error) {
-	return &GetTypesArguments{
-		UUID:     uuid.MustParse("b085b0b9-007c-440e-9661-0d8f2de98a5a"),
-		Bool:     true,
-		String:   "hello",
-		Int:      1,
-		Int8:     2,
-		Int16:    3,
-		Int32:    4,
-		Int64:    5,
-		Uint:     6,
-		Uint8:    7,
-		Uint16:   8,
-		Uint32:   9,
-		Uint64:   10,
-		Float32:  1.1,
-		Float64:  2.2,
-		Time:     time.Date(2023, 3, 5, 7, 0, 56, 0, time.UTC),
-		Duration: 10 * time.Second,
-		CustomScalar: CommentText{
-			comment: "a comment",
-		},
-		UUIDPtr:     utils.ToPtr(uuid.MustParse("b085b0b9-007c-440e-9661-0d8f2de98a5b")),
-		BoolPtr:     utils.ToPtr(true),
-		IntPtr:      utils.ToPtr(11),
-		Int8Ptr:     utils.ToPtr(int8(12)),
-		Int16Ptr:    utils.ToPtr(int16(13)),
-		Int32Ptr:    utils.ToPtr(int32(14)),
-		Int64Ptr:    utils.ToPtr(int64(15)),
-		UintPtr:     utils.ToPtr(uint(16)),
-		Uint8Ptr:    utils.ToPtr(uint8(17)),
-		Uint16Ptr:   utils.ToPtr(uint16(18)),
-		Uint32Ptr:   utils.ToPtr(uint32(19)),
-		Uint64Ptr:   utils.ToPtr(uint64(20)),
-		Float32Ptr:  utils.ToPtr(float32(3.3)),
-		Float64Ptr:  utils.ToPtr(float64(4.4)),
-		TimePtr:     utils.ToPtr(time.Date(2023, 3, 5, 7, 0, 0, 0, time.UTC)),
-		DurationPtr: utils.ToPtr(time.Minute),
-		CustomScalarPtr: &CommentText{
-			comment: "a comment pointer",
-		},
-		Object: struct {
-			ID        uuid.UUID `json:"id"`
-			CreatedAt time.Time `json:"created_at"`
-		}{
-			ID:        uuid.MustParse("b085b0b9-007c-440e-9661-0d8f2de98a5c"),
-			CreatedAt: time.Date(2023, 3, 5, 6, 0, 0, 0, time.UTC),
-		},
-		ObjectPtr: &struct {
-			Long int
-			Lat  int
-		}{
-			Long: 1,
-			Lat:  2,
-		},
-		ArrayObject: []struct {
-			Content string "json:\"content\""
-		}{
-			{
-				Content: "a content",
-			},
-		},
-		ArrayObjectPtr: &[]struct {
-			Content string "json:\"content\""
-		}{
-			{
-				Content: "a content pointer",
-			},
-		},
-		NamedObject: Author{
-			ID:        "1",
-			Duration:  10 * time.Minute,
-			CreatedAt: time.Date(2023, 3, 5, 5, 0, 0, 0, time.UTC),
-		},
-		NamedObjectPtr: &Author{
-			ID:        "2",
-			Duration:  11 * time.Minute,
-			CreatedAt: time.Date(2023, 3, 5, 4, 0, 0, 0, time.UTC),
-		},
-		NamedArray: []Author{
-			{
-				ID:        "3",
-				Duration:  12 * time.Minute,
-				CreatedAt: time.Date(2023, 3, 5, 3, 0, 0, 0, time.UTC),
-			},
-		},
-		NamedArrayPtr: nil,
-	}, nil
+	return arguments, nil
 }
