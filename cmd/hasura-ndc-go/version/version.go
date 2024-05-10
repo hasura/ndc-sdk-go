9cb3e949833adfd1990152ae6d174c786d14a064
Toan Nguyen <hgiasac@gmail.com>
2024-05-11 00:24:38 +0700

    support native packaging spec (#90)

--- cmd/hasura-ndc-go/version/version.go
+++ cmd/hasura-ndc-go/version/version.go
@@ -1,9 +1,35 @@
 // Package version implements cli handling.
 package version
 
+import (
+	"runtime/debug"
+)
+
 // DevVersion is the version string for development versions.
-const DevVersion = "dev"
+const DevVersion = "v0"
 
 // BuildVersion is the version string with which CLI is built. Set during
 // the build time.
-var BuildVersion = DevVersion
+var BuildVersion = ""
+
+func init() {
+	initBuildVersion()
+}
+
+func initBuildVersion() {
+	if BuildVersion != "" {
+		return
+	}
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		BuildVersion = DevVersion
+		return
+	}
+
+	for _, s := range bi.Settings {
+		if s.Key == "vcs.revision" {
+			BuildVersion = s.Value
+			return
+		}
+	}
+}
