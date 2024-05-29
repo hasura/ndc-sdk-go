5643642211c2971e7a5b42df6ed3e53f7c9354ec
Toan Nguyen <hgiasac@gmail.com>
2024-05-30 01:09:26 +0700

    upgrade otel/semconv/v1.25.0

--- connector/telemetry.go
+++ connector/telemetry.go
@@ -28,7 +28,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
 	traceapi "go.opentelemetry.io/otel/trace"
 )
 
