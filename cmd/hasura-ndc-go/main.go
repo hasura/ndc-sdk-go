ebee0c08c94ef782b1f22f41327218d4756de300
Toan Nguyen <hgiasac@gmail.com>
2024-07-04 23:52:21 +0700

    cmd: prepare cli plugin (#128)

--- cmd/hasura-ndc-go/main.go
+++ cmd/hasura-ndc-go/main.go
@@ -12,8 +12,9 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-type GenerateArguments struct {
-	Path         string   `help:"The path of the root directory where the go.mod file is present" short:"p" default:"."`
+// UpdateArguments represent input arguments of the `update` command
+type UpdateArguments struct {
+	Path         string   `help:"The path of the root directory where the go.mod file is present" short:"p" env:"HASURA_PLUGIN_CONNECTOR_CONTEXT_PATH" default:"."`
 	ConnectorDir string   `help:"The directory where the connector.go file is placed" default:"."`
 	PackageTypes string   `help:"The name of types package where the State struct is in"`
 	Directories  []string `help:"Folders contain NDC operation functions" short:"d"`
@@ -28,9 +29,10 @@ type NewArguments struct {
 }
 
 var cli struct {
-	LogLevel string            `help:"Log level." enum:"debug,info,warn,error" default:"info"`
-	New      NewArguments      `cmd:"" help:"Initialize an NDC connector boilerplate. For example:\n hasura-ndc-go new -n example -m github.com/foo/example"`
-	Generate GenerateArguments `cmd:"" help:"Generate schema and implementation for the connector from functions."`
+	LogLevel string          `help:"Log level." enum:"debug,info,warn,error" env:"HASURA_PLUGIN_LOG_LEVEL" default:"info"`
+	New      NewArguments    `cmd:"" help:"Initialize an NDC connector boilerplate. For example:\n hasura-ndc-go new -n example -m github.com/foo/example"`
+	Update   UpdateArguments `cmd:"" help:"Generate schema and implementation for the connector from functions."`
+	Generate UpdateArguments `cmd:"" help:"(deprecated) The alias of the 'update' command."`
 	Test     struct {
 		Snapshots command.GenTestSnapshotArguments `cmd:"" help:"Generate test snapshots."`
 	} `cmd:"" help:"Test helpers."`
@@ -61,30 +63,10 @@ func main() {
 		}
 		log.Info().Str("exec_time", time.Since(start).Round(time.Second).String()).
 			Msg("generated successfully")
+	case "update":
+		execUpdate(&cli.Update, start)
 	case "generate":
-		log.Info().
-			Str("path", cli.Generate.Path).
-			Str("connector_dir", cli.Generate.ConnectorDir).
-			Str("package_types", cli.Generate.PackageTypes).
-			Msg("generating connector schema...")
-
-		moduleName, err := getModuleName(cli.Generate.Path)
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to get module name. The base path must contain a go.mod file")
-		}
-
-		if err := os.Chdir(cli.Generate.Path); err != nil {
-			log.Fatal().Err(err).Msg("")
-		}
-
-		if err = parseAndGenerateConnector(&cli.Generate, moduleName); err != nil {
-			log.Fatal().Err(err).Msg("failed to generate connector schema")
-		}
-		if err := execGoFormat("."); err != nil {
-			log.Fatal().Err(err).Msg("failed to format code")
-		}
-		log.Info().Str("exec_time", time.Since(start).Round(time.Millisecond).String()).
-			Msg("generated successfully")
+		execUpdate(&cli.Generate, start)
 	case "test snapshots":
 		log.Info().
 			Str("endpoint", cli.Test.Snapshots.Endpoint).
@@ -100,6 +82,32 @@ func main() {
 	default:
 		log.Fatal().Msgf("unknown command <%s>", cmd.Command())
 	}
+}
+
+func execUpdate(args *UpdateArguments, start time.Time) {
+	log.Info().
+		Str("path", args.Path).
+		Str("connector_dir", args.ConnectorDir).
+		Str("package_types", args.PackageTypes).
+		Msg("generating connector schema...")
+
+	moduleName, err := getModuleName(args.Path)
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to get module name. The base path must contain a go.mod file")
+	}
+
+	if err := os.Chdir(args.Path); err != nil {
+		log.Fatal().Err(err).Msg("")
+	}
+
+	if err = parseAndGenerateConnector(args, moduleName); err != nil {
+		log.Fatal().Err(err).Msg("failed to generate connector schema")
+	}
+	if err := execGoFormat("."); err != nil {
+		log.Fatal().Err(err).Msg("failed to format code")
+	}
+	log.Info().Str("exec_time", time.Since(start).Round(time.Millisecond).String()).
+		Msg("generated successfully")
 }
 
 func setupGlobalLogger(level string) {
