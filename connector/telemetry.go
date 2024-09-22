4fff7ae4740bc7ab763646d2bb8ae2bba531a9b2
Toan Nguyen <hgiasac@gmail.com>
2024-09-22 09:51:10 +0700

    add NewTracer function

--- connector/telemetry.go
+++ connector/telemetry.go
@@ -380,6 +380,13 @@ type Tracer struct {
 
 var _ traceapi.Tracer = &Tracer{}
 
+// NewTracer creates a new OpenTelemetry tracer
+func NewTracer(name string, opts ...traceapi.TracerOption) *Tracer {
+	return &Tracer{
+		Tracer: otel.Tracer(name, opts...),
+	}
+}
+
 // Start creates a span and a context.Context containing the newly-created span
 // with `internal.visibility: "user"` so that it shows up in the Hasura Console.
 func (t *Tracer) Start(ctx context.Context, spanName string, opts ...traceapi.SpanStartOption) (context.Context, traceapi.Span) {
