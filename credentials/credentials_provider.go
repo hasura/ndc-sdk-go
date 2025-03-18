4e03ea9b387dd903af9f81995a96db62799d547c
Toan Nguyen <hgiasac@gmail.com>
2025-03-18 20:13:38 +0700

    wrap AccquireCredentials function into a client

--- credentials/credentials_provider.go
+++ credentials/credentials_provider.go
@@ -1,48 +1,81 @@
-package connector
+package credentials
 
 import (
 	"errors"
-	"os"
-
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 )
 
-var (
-	errAuthWebhookUriRequired = errors.New("the env var HASURA_CREDENTIALS_PROVIDER_URI must be set and non-empty")
-)
+var errAuthWebhookUriRequired = errors.New("the env var HASURA_CREDENTIALS_PROVIDER_URI must be set and non-empty")
 
-// AccquireCredentials calls the credentials provider webhook to get the credentials for the given key.
+var defaultClient = CredentialClient{
+	httpClient: http.DefaultClient,
+}
+
+// AcquireCredentials calls the credentials provider webhook to get the credentials for the given key.
 // If force_refresh is true, the provider will ignore any cached credentials and fetch new ones.
 // The credentials provider URI is read from the HASURA_CREDENTIALS_PROVIDER_URI environment variable.
 // If the HASURA_CREDENTIALS_PROVIDER_BEARER_TOKEN environment variable is set, it will be used as a bearer token for the request.
-func AccquireCredentials(key string, force_refresh bool) (string, error) {
-	authWebhookUri, authWebhookUriExists := os.LookupEnv("HASURA_CREDENTIALS_PROVIDER_URI")
-	if !authWebhookUriExists || authWebhookUri == "" {
-		return "", errAuthWebhookUriRequired
+func AcquireCredentials(key string, forceRefresh bool) (string, error) {
+	return defaultClient.AcquireCredentials(key, forceRefresh)
+}
+
+// CredentialClient is an HTTP client that  can requests the credentials provider webhook to get the credentials.
+type CredentialClient struct {
+	providerUri         string
+	providerBearerToken string
+	httpClient          *http.Client
+}
+
+// NewCredentialClient creates a CredentialClient instance.
+func NewCredentialClient(httpClient *http.Client) (*CredentialClient, error) {
+	client := &CredentialClient{
+		httpClient: httpClient,
 	}
-	bearerToken, bearerTokenExists := os.LookupEnv("HASURA_CREDENTIALS_PROVIDER_BEARER_TOKEN")
+
+	return client, client.reload()
+}
+
+func (cc *CredentialClient) reload() error {
+	providerUri, providerUriExists := os.LookupEnv("HASURA_CREDENTIALS_PROVIDER_URI")
+	if !providerUriExists || providerUri == "" {
+		return errAuthWebhookUriRequired
+	}
+
+	cc.providerUri = providerUri
+	cc.providerBearerToken = os.Getenv("HASURA_CREDENTIALS_PROVIDER_BEARER_TOKEN")
+
+	return nil
+}
+
+// AcquireCredentials calls the credentials provider webhook to get the credentials for the given key.
+func (cc *CredentialClient) AcquireCredentials(key string, forceRefresh bool) (string, error) {
+	if forceRefresh || cc.providerUri == "" {
+		if err := cc.reload(); err != nil {
+			return "", err
+		}
+	}
 
 	params := url.Values{}
 	params.Add("key", key)
-	params.Add("force_refresh", strconv.FormatBool(force_refresh))
+	params.Add("force_refresh", strconv.FormatBool(forceRefresh))
 
-	fullWebhookUri := fmt.Sprintf("%s?%s", authWebhookUri, params.Encode())
+	fullWebhookUri := fmt.Sprintf("%s?%s", cc.providerUri, params.Encode())
 
-	req, err := http.NewRequest("GET", fullWebhookUri, nil)
+	req, err := http.NewRequest(http.MethodGet, fullWebhookUri, nil)
 	if err != nil {
 		return "", fmt.Errorf("error creating request: %w", err)
 	}
 
-	if bearerTokenExists {
-		req.Header.Set("Authorization", "Bearer "+bearerToken)
+	if cc.providerBearerToken != "" {
+		req.Header.Set("Authorization", "Bearer "+cc.providerBearerToken)
 	}
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := cc.httpClient.Do(req)
 	if err != nil {
 		return "", fmt.Errorf("error making request: %w", err)
 	}
--- credentials/credentials_provider_test.go
+++ credentials/credentials_provider_test.go
@@ -1,4 +1,4 @@
-package connector
+package credentials
 
 import (
 	"fmt"
@@ -8,12 +8,12 @@ import (
 	"testing"
 )
 
-func TestAccquireCredentials(t *testing.T) {
+func TestAcquireCredentials(t *testing.T) {
 	t.Run("when HASURA_CREDENTIALS_PROVIDER_URI is not set", func(t *testing.T) {
 		// Unset the environment variable
 		os.Unsetenv("HASURA_CREDENTIALS_PROVIDER_URI")
 
-		_, err := AccquireCredentials("key", false)
+		_, err := AcquireCredentials("key", false)
 		if err != errAuthWebhookUriRequired {
 			t.Errorf("expected error %v, got %v", errAuthWebhookUriRequired, err)
 		}
@@ -32,7 +32,7 @@ func TestAccquireCredentials(t *testing.T) {
 			// Set the environment variable
 			os.Setenv("HASURA_CREDENTIALS_PROVIDER_URI", server.URL)
 
-			credentials, err := AccquireCredentials("key", false)
+			credentials, err := AcquireCredentials("key", false)
 			if err != nil {
 				t.Errorf("unexpected error: %v", err)
 			}
@@ -44,7 +44,7 @@ func TestAccquireCredentials(t *testing.T) {
 		t.Run("when the request fails", func(t *testing.T) {
 			os.Setenv("HASURA_CREDENTIALS_PROVIDER_URI", "http://localhost:0000")
 
-			_, err := AccquireCredentials("key", false)
+			_, err := AcquireCredentials("key", false)
 			if err == nil {
 				t.Error("expected an error, got nil")
 			}
